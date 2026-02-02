951473d2b24184678e53094b152277a68051e64c
hailangzz <344267342@qq.com>
2026-02-02 21:10:47 +0800

    调整fov缩小至托盘大小

---
+++ My_AI_interface/yolov8_HandDetectProject/cpp/include/version.h
@@ -0,0 +1,4 @@
+#pragma once
+
+#define LIBRARY_VERSION "HandDetect:1.0.0"
+#define BUILD_TIMESTAMP "2026-02-02_20-49-58"
--- My_AI_interface/yolov8_HandDetectProject/cpp/src/hand_detect_interface.cc
+++ My_AI_interface/yolov8_HandDetectProject/cpp/src/hand_detect_interface.cc
@@ -8,6 +8,7 @@
 #include "image_drawing.h"
 #include "event_control.h"
 #include "logs.h"
+// #include "version.h"
 
 #include <mutex>
 #include <cstdio>    // 用于 snprintf
@@ -37,6 +38,13 @@
 //         } while(0)
 // #endif
 
+
+// void print_version() {
+//     std::cout << "Library Version: " << LIBRARY_VERSION << std::endl;
+//     std::cout << "Build Timestamp: " << BUILD_TIMESTAMP << std::endl;
+// }
+
+
 // ====================== 配置路径 & 读取 ======================
 static std::string& getConfigPath() {
     static std::string config_path = "./config/cfg.txt";  // 默认路径
@@ -51,6 +59,10 @@ static ConfigInfo& getConfig()
 
 void hand_detect_set_config_path(const std::string& path) {
     getConfigPath() = path;
+ 
+    // 输出版本号和构建时间戳
+    LOGI("Library Version: %s", LIBRARY_VERSION);
+    LOGI("Build Timestamp: %s", BUILD_TIMESTAMP);
 }
 
 // ====================== 单例模块 ======================
