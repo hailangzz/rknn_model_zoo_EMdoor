9fa5f083b81962ea9d0890db70c1c1e3b28c3283
hailangzz <344267342@qq.com>
2026-06-04 13:43:50 +0800

    线材检测上，适配模型板端连续推理结果存储

--- My_AI_interface/yolov8_WireSegmentProject/cpp/include/detect_context.h
+++ My_AI_interface/yolov8_WireSegmentProject/cpp/include/detect_context.h
@@ -2,12 +2,14 @@
 #pragma once
 #include "yolov8_detect.h"
 #include "transform_coordinates.h"
+#include "debug.h"
 
 struct DetectContext {
     bool initialized = false;
     ConfigInfo config;
     Detector* detector = nullptr;
     CameraParameters* camera_params = nullptr;
+    Debug* debuger = nullptr;
 };
 
 
--- My_AI_interface/yolov8_WireSegmentProject/cpp/include/types.h
+++ My_AI_interface/yolov8_WireSegmentProject/cpp/include/types.h
@@ -63,6 +63,10 @@ struct ConfigInfo {
     float camera_D_5;
     float camera_D_6;
     float camera_D_7;
+    float debug_score_threshold;
+    std::string save_debug_images_path;
+    bool is_save_debug_images;
+    int fps_limit;
 
 };
 
